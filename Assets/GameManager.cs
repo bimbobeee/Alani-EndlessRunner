1ef8151cdca94808074456cec435746cac5e0d72
bimbobeee <Alanit.tufts@gmail.com>
2024-03-04 16:05:25 -0500

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager platformPrefab;
+
+    public int platformCount = 250;
+
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+        
+        for (int i = 0; ik < platformCount; i++)
+    }
+}
